2af46b02cf9c7c3c34e2bc7b8481bce6f48fd4cf
Louis Andrew <louisandrew3@gmail.com>
2020-11-20 10:25:20 +0100

    Added homepage template and testing deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 import Layout from 'layout'
 import { SEO } from 'components/seo'
-import Box from 'components/box'
 
 const App = () => (
     <Layout>
         <SEO />
-        <Box />
+        {/* <Box /> */}
     </Layout>
 )
 
---
+++ src/templates/homepage/homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import styling libs
+// import local components
+
+/**
+ * Used as a wrapper element for all sections in the homepage
+ */
+const Homepage: React.FC<unknown> = () => {
+    return <></>
+}
+
+export { Homepage }
