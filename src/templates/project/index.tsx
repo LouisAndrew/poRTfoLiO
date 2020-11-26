9c2da5a2f4a3bfa357a64be09dd3d7bbdbc276a3
Louis Andrew <louisandrew3@gmail.com>
2020-11-26 22:59:46 +0100

    Done sourcing from markdown

--- src/templates/project/index.tsx
+++ src/templates/project/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-// import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import { Project } from './project'
 import Layout from 'layout'
 
-const ProjectPage = () => {
+const ProjectPage = (props: any) => {
+    const {
+        data: { projectData },
+    } = props
+
+    console.log(projectData)
+
     return (
         <Layout>
             <Project />
@@ -12,6 +18,32 @@ const ProjectPage = () => {
     )
 }
 
-// export const query = graphql``
+export const query = graphql`
+    query($name: String!) {
+        projectData: markdownRemark(
+            frontmatter: { project_name: { eq: $name } }
+        ) {
+            frontmatter {
+                desc
+                finished_date(formatString: "LL")
+                preview_desc
+                project_name
+                project_screenshots {
+                    screenshot_label
+                    screenshot {
+                        childImageSharp {
+                            fluid(maxWidth: 400, quality: 100) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }
+                techs
+                web_url
+                repo_url
+            }
+        }
+    }
+`
 
 export default ProjectPage
