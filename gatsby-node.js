29b07db730bf43cb7fb82c366459127400076ad2
Louis Andrew <louisandrew3@gmail.com>
2020-11-26 22:48:59 +0100

    Sourced data from markdown.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,58 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path')
+// const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// exports.onCreateNode = ({ node, getNode }) => {
+//     // search for the MarkdownRemark type of node(s)
+//     if (node.internal.type === `MarkdownRemark`) {
+//         console.log(createFilePath({ node, getNode, basePath: `pages` }))
+//     }
+// }
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    const nameToSlug = name =>
+        name
+            .toLowerCase()
+            .split(' ')
+            .join('_')
+
+    // **Note:** The graphql function call returns a Promise
+    // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+    const result = await graphql(`
+        query {
+            allMarkdownRemark(
+                filter: { frontmatter: { template_key: { eq: "project" } } }
+            ) {
+                edges {
+                    node {
+                        frontmatter {
+                            project_name
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    // console.log(JSON.stringify(result, null, 4))
+
+    const projects = await result.data.allMarkdownRemark.edges.map(
+        edge => edge.node.frontmatter.project_name
+    )
+
+    projects.forEach(projectName => {
+        createPage({
+            path: nameToSlug(projectName),
+            component: path.resolve(`./src/templates/project/index.tsx`),
+            context: {
+                // Data passed to context is available
+                // in page queries as GraphQL variables.
+                name: projectName,
+            },
+        })
+    })
+}
---
+++ src/templates/project/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { graphql } from 'gatsby'
+
+import { Project } from './project'
+import Layout from 'layout'
+
+const ProjectPage = () => {
+    return (
+        <Layout>
+            <Project />
+        </Layout>
+    )
+}
+
+// export const query = graphql``
+
+export default ProjectPage
