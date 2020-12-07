59d6b5292afce7c4fdaf9ccaa0aba38b1d64f089
Louis Andrew <louisandrew3@gmail.com>
2020-12-07 17:00:13 +0100

    trying to change the footer scroll trigger (again)

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -23,8 +23,8 @@ const Footer: React.FC<unknown> = () => {
             TweenLite.to('#header-link-contact', 0.4, {
                 scrollTrigger: {
                     trigger: el,
-                    start: '+=100',
-                    end: '-=100',
+                    start: '-=100',
+                    end: '-=150',
                     toggleActions: 'play reverse play reverse',
                 },
                 opacity: 1,
